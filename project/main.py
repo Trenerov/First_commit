9cbf9d556cbfdefb00e0736e96ec7f9067982057
Trenerov <sustanon92@gmail.com>
2025-04-11 16:18:22 +0300

    Initial commit

--- project/main.py
+++ project/main.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+note_storage = {}
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/notes", methods=["GET", "POST"])
+def notes():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        if title and content:
+            note_storage[title] = content
+        return redirect(url_for('notes'))
+    
+    return render_template("notes.html", notes=note_storage)
 
 if __name__ == "__main__":
     app.run(debug=True)
